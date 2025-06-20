3f1835dc20fb9b10beb79b744ff18ceb27ad010f
Juan Hubert Liem <juanhl8010@gmail.com>
2025-06-20 15:00:31 +0700

    Introduction update AM NZ

---
+++ Hand Sign Language/Camera/A-MHandPathView.swift
@@ -0,0 +1,18 @@
+//
+//  A-MHandPathView.swift
+//  Hand Sign Language
+//
+//  Created by Juan Hubert Liem on 20/06/25.
+//
+
+import SwiftUI
+
+struct A_MHandPathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    A_MHandPathView()
+}
---
+++ Hand Sign Language/Camera/NZHandPathView.swift
@@ -0,0 +1,18 @@
+//
+//  NZHandPathView.swift
+//  Hand Sign Language
+//
+//  Created by Juan Hubert Liem on 20/06/25.
+//
+
+import SwiftUI
+
+struct NZHandPathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NZHandPathView()
+}
